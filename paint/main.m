d4fcea2d943402af72bb32c6f3b6bf9b9595b39e
Jari Kalinainen <jari@klubitii.fi>
2015-12-05 17:52:45 +0200

    Initial Commit

---
+++ paint/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  paint
+//
+//  Created by Jari Kalinainen on 05.12.15.
+//  Copyright © 2015 Jari Kalinainen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
